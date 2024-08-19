7abdf9ffffe1aafac7151a157c8fbb6e472bd146
Sofia <sofia.stazi04@gmail.com>
2024-08-19 12:00:17 -0300

    solución error variables

--- js/validacion.js
+++ js/validacion.js
@@ -6,15 +6,15 @@ function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
 
-// Obtener valores de los campos
-const nombre = document.getElementById("nombre").value;
-const apellido = document.getElementById("apellido").value;
-const email = document.getElementById("email").value;
-const password1 = document.getElementById("password1").value;
-const password2 = document.getElementById("password2").value;
-const terminos = document.getElementById("terminos").checked;
 
 document.getElementById("regBtn").addEventListener("click", function () {
+    // Obtener valores de los campos
+    const nombre = document.getElementById("nombre").value;
+    const apellido = document.getElementById("apellido").value;
+    const email = document.getElementById("email").value;
+    const password1 = document.getElementById("password1").value;
+    const password2 = document.getElementById("password2").value;
+    const terminos = document.getElementById("terminos").checked;
  // Validaciones
  if (nombre == "" || apellido == "" || email == "" || password1 == "" || password2 == "" || password1 !== password2 || password1.length < 6 || !terminos) {
     showAlertError();
