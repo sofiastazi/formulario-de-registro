506424445feedefbce915974ca6411202ac84f01
Sofia <sofia.stazi04@gmail.com>
2024-08-19 11:50:04 -0300

    almacenar los valores de los campos en variables

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,11 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+// Obtener valores de los campos
+const nombre = document.getElementById("nombre").value;
+const apellido = document.getElementById("apellido").value;
+const email = document.getElementById("email").value;
+const password1 = document.getElementById("password1").value;
+const password2 = document.getElementById("password2").value;
+const terminos = document.getElementById("terminos").checked;
